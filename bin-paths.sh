1ca671927cf8bd16dcf8f700a5efc1c6c5b1288e
Jake Hartz <jake+git@hartz.io>
2017-03-11 19:55:09 -0500

    non-bash compatibility

--- bin-paths.sh
+++ bin-paths.sh
@@ -3,8 +3,9 @@
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for BIN_DIR in "$THIS_DIR"/bin*; do
-    export PATH="$PATH:$BIN_DIR"
+    PATH="$PATH:$BIN_DIR"
 done
+export PATH
 
 unset BIN_DIR
 unset THIS_DIR
