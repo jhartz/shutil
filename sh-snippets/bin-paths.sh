6a938991964c1b1f5c4a16d09015bc0c12595fb3
Jake Hartz <jake-git@hartz.io>
2023-04-09 23:15:24 -0400

    Make bin-paths.sh a bit friendlier to non-bash shells

--- sh-snippets/bin-paths.sh
+++ sh-snippets/bin-paths.sh
@@ -1,14 +1,15 @@
 # Add the various "bin" directories to our PATH
 
-REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+if [ "$BASH_SOURCE" ]; then
+    REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-for BIN_DIR in "$REPO_ROOT"/bin*; do
-    if [ -d "$BIN_DIR" ]; then
-        PATH="$PATH:$BIN_DIR"
-    fi
-done
-export PATH
+    for BIN_DIR in "$REPO_ROOT"/bin*; do
+        if [ -d "$BIN_DIR" ]; then
+            PATH="$PATH:$BIN_DIR"
+        fi
+    done
+    export PATH
 
-unset BIN_DIR
-unset REPO_ROOT
-
+    unset BIN_DIR
+    unset REPO_ROOT
+fi
